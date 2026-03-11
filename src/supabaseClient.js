08a9be728eb7f2166d8c77fb6bc5906f6da67f31
mfurlanetto <marco.furlanetto@yahoo.com>
2026-03-11 14:28:59 +0100

    env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://jmepahxupsqlogtiahtq.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_NlpgcY4T3fL1SzzPULSFVw_JSfw3sX8'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
